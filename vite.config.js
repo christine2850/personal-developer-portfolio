0a15e8482431a307c88369b58f8382046f89b689
Christine Joy Aldea <christinejoy.aldea250206@gmail.com>
2026-02-01 20:22:18 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/personal-developer-portfolio",
-  assetsInclude: ["**/*.pdf"],
+  assetsInclude: ["**/*.pdf"], //pdf
 });
